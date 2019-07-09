fedbd71ee41d289a2b8a523a5b09a85190d6ba10
Thom <tomdagz@gmail.com>
2019-07-09 14:58:18 -0500

    added method to get products

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+const PRODUCTS_URL = 'http://test.oneclean.mx/test.php';
+
+window.addEventListener('load', init, false);
+
+function listProducts(data) {
+    let list = '';
+
+    for(let product of data.products) {
+        list += `
+            <div class="col-sm-6 col-md-4 col-lg-3 product-info">
+                <div class="card">
+                    <div class="card-body">
+                        <h5>${ product.name }</h5>
+                        <form class="product-form">
+                            <div class="form-group">
+                                <label for="quantity">Cantidad</label>
+                                <input type="number" max="${ product.available }" id="quantity" class="form-control" required>
+                                <small id="" class="text-muted">${ product.available } disponibles</small>
+                            </div>
+                            <input type="submit" value="Agregar al carrito" class="btn btn-primary btn-product">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+
+    document.getElementsByClassName('loading')[0].innerHTML = '';
+    document.getElementById('products').innerHTML = list;
+}
+
+function showProducts() {
+    fetch(PRODUCTS_URL)
+    .then(response => response.json())
+    .then(data => {
+        listProducts(data);
+    })
+    .catch(error => console.error(error))
+}
+
+function init() {
+    showProducts()
+}
