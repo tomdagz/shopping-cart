4515ee730418b4fa2735b36f53876293a0cf74f0
Thom <tomdagz@gmail.com>
2019-07-09 14:34:15 -0500

    added own assets

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 app.set('view engine', 'pug');
+app.use(express.static('assets'));
 
 app.get('/', function(request, response) {
     response.render('index');
