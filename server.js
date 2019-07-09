1deb9bb5d1a416a41ac0e9c04a21a85ae7f62b0c
Thom <tomdagz@gmail.com>
2019-07-09 14:29:52 -0500

    added pug templates

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
 
 const app = express();
+app.set('view engine', 'pug');
+
+app.get('/', function(request, response) {
+    response.render('index');
+});
 
 app.listen(3000);
