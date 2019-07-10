8a976ee07e3c6ae1997a3c384178bfd031cb1d47
Thom <tomdagz@gmail.com>
2019-07-09 20:07:33 -0500

    added table for shopping resume

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,25 +8,33 @@ function listProducts(data) {
     let list = '';
 
     for(let product of data.products) {
-        colors = '<option selected value="">Selecciona el color</option>';
+        let colors = '<option selected value="">Seleccione</option>';
 
         product.colors.forEach(function(color) {
-            colors += `<option value="${ color.nameColor }">${ color.nameColor }</option>`
+            let sizes = '';
+
+            color.size.forEach(function(size) {
+                sizes += `<option value="${ size.name }" data-price="${ size.quantity }">${ size.name } $${ size.quantity }</option>`;
+            });
+
+            colors += `<optgroup label="${ color.nameColor }" data-color="${ color.nameColor }">
+                ${ sizes }
+            </optgroup>`
         });
 
         list += `
             <div class="col-sm-6 col-md-4 col-lg-3 product-info">
                 <div class="card">
                     <div class="card-body">
-                        <h5>${ product.name }</h5>
                         <form class="product-form" data-product-id=${ product.id }>
+                            <h5>${ product.name }</h5>
                             <div class="form-group">
                                 <label for="quantity">Cantidad</label>
-                                <input name="quantity" type="number" max="${ product.available }" id="quantity" class="form-control" required data-stock=${ product.available }>
+                                <input name="quantity" type="number" min="1" max="${ product.available }" id="quantity" class="form-control" required data-stock=${ product.available }>
                                 <small id="" class="text-muted">${ product.available } disponibles</small>
                             </div>
                             <div class="form-group">
-                                <label for="color">Color</label>
+                                <label for="color">Color y talla</label>
                                 <select class="custom-select" id="color" name="color" required>
                                     ${ colors }
                                 </select>
@@ -54,14 +62,44 @@ function showProducts() {
 
 function addToCart(e) {
     e.preventDefault();
+    const colorSize =  $(this).find('select[name="color"]');
+    const option = colorSize.find('option:selected');
 
     const order_info = {
         id: $(this).attr('data-product-id'),
         amount: $(this).find('input[name="quantity"]').val(),
-        color: $(this).find('select[name="color"]').val(),
-        //size: 
+        size: colorSize.val(),
+        color: option.closest('optgroup').attr('data-color'),
+        price: option.attr('data-price'),
+        name: $(this).find('h5').text(),
     }
-    storage.addProduct(order_info);
+    if (storage.addProduct(order_info)) {
+        updateShoppingCart();
+    }
+}
+
+function updateShoppingCart() {
+    const products = storage.getProducts();
+    let rows = ''
+    let total = 0
+
+    products.forEach(function(product) {
+        const price = parseFloat(product.amount) * parseFloat(product.price);
+        total += price;
+
+        rows += `<tr>
+            <td>${ product.amount }</td>
+            <td>${ product.name } ${ product.size } ${ product.color }</td>
+            <td>$${ price }</td>
+        </tr>`
+    });
+
+    rows += `<tr>
+        <td colspan="2">Total</td>
+        <td>$${ total }</td>
+    </tr>`
+
+    document.getElementById('cart-table').innerHTML = rows;
 }
 
 function init() {
